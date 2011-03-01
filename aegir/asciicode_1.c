2f668990b39c204eaf69d8685a7ffc1a9540ae58
yongeon.park <yongeon.park@gmail.com>
2011-03-01 21:38:26 +0900

    다시

--- aegir/asciicode_1.c
+++ aegir/asciicode_1.c
@@ -2,7 +2,20 @@
 
 void main()
 {
-	int i = 0, j = 0, k = 0;
+	int i = 0, j = 0, k = 0, l = 0;
+	for( l = 0; l < 6; ++l)
+	{
+		printf("16: 10:c | ");
+	}
+	printf("\n");
+	printf("---------+");
+	
+	for( l = 0; l < 5; ++l)
+	{
+		printf("----------+");
+	}
+	printf("\n");
+
 	for( k = 0; k < 16; ++k)
 	{
 		j = 32 + k;
